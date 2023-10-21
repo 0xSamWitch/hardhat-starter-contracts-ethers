f34be5b80ff1f0b2d10aa56409437b989ac60c14
0xSamWitch <julian.assange12@outlook.com>
2023-10-21 18:49:07 +0100

    Add votes test

--- test/OFTERC20.ts
+++ test/OFTERC20.ts
@@ -2,6 +2,7 @@ import {loadFixture} from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import {ethers} from "hardhat";
 import {expect} from "chai";
 import {TOKEN_CONTRACT_NAME} from "../constants/contracts";
+import {Block} from "ethers";
 
 describe("OFTERC20", function () {
   async function deployContractsFixture() {
@@ -82,6 +83,47 @@ describe("OFTERC20", function () {
     );
     await oft.recoverToken(oft, ethers.parseEther("100"));
     expect(await oft.balanceOf(owner)).to.eq(ethers.parseEther("1000"));
+  });
+
+  describe("Votes", function () {
+    async function deployContractsFixture() {
+      const [owner, alice, bob] = await ethers.getSigners();
+
+      const chainId = (await ethers.provider.getNetwork()).chainId;
+      const lzEndpoint = await ethers.deployContract("LZEndpointMock", [chainId]);
+
+      const oft = await ethers.deployContract(TOKEN_CONTRACT_NAME);
+      await oft.initialize(lzEndpoint);
+
+      return {oft, owner, alice, bob, lzEndpoint};
+    }
+
+    it("Recent checkpoints", async function () {
+      const {oft, alice} = await loadFixture(deployContractsFixture);
+
+      await oft.connect(alice).delegate(alice);
+      for (let i = 0; i < 6; ++i) {
+        await oft.transfer(alice, 1);
+      }
+      const {number: timepoint} = (await ethers.provider.getBlock("latest")) as Block;
+
+      expect(await oft.numCheckpoints(alice)).to.equal(6);
+      // recent
+      expect(await oft.getPastVotes(alice, timepoint - 1)).to.eq(5);
+      // non-recent
+      expect(await oft.getPastVotes(alice, timepoint - 6)).to.eq(0);
+    });
+
+    it("Transfer", async function () {
+      const {oft, alice, bob} = await loadFixture(deployContractsFixture);
+
+      await oft.connect(alice).delegate(alice);
+      await oft.connect(bob).delegate(bob);
+      await oft.transfer(alice, 1);
+      await oft.connect(alice).transfer(bob, 1);
+      expect(await oft.getVotes(alice)).to.eq(0);
+      expect(await oft.getVotes(bob)).to.eq(1);
+    });
   });
 
   it("supportsInterface", async function () {
